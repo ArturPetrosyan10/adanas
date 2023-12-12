dfd2271a77c6e281233adc52eb280175b6a680ef
ArturPetrosyan10 <artur-petrosyan-2022@mail.ru>
2023-12-12 15:53:55 +0400

    before news direction

---
+++ views/site/news.php
@@ -0,0 +1,8 @@
+<div class="fs-breadcrumbs-wrapper">
+    <div class="fs-container">
+        <ul class="fs-breadcrumbs-list">
+            <li class="fs-breadcrumbs-el"><a href="/"><?=$GLOBALS['text']['__home__page__']?></a></li>
+            <li class="fs-breadcrumbs-el"><?=$GLOBALS['text']['__news__']?></li>
+        </ul>
+    </div>
+</div>
---
+++ web/js/new/app.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $('body').on('click','.fs-nav-burger',function () {
+        $('.fs-hidden-menu-list-wrapper-ul ').toggleClass('d-none');
+    })
+})
