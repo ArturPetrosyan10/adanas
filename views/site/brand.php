4c0fc9b007e3e2f06726396ec57f2a66807010e2
ArturPetrosyan10 <artur-petrosyan-2022@mail.ru>
2023-12-18 12:40:43 +0400

    pagination block and main pages

---
+++ views/site/brand.php
@@ -0,0 +1,53 @@
+<style>
+    .brand-block img{
+        width:78%;
+        aspect-ratio: 3/2;
+        object-fit:contain;
+        max-height:100%;
+        /*mix-blend-mode:color-burn;*/
+    }
+     .brand-block {
+         background-color: #ffffff;
+         transition: 0.3s;
+         border-radius: 4px;
+         border: 1px solid #e5e8ec;
+         padding: 5px;
+         cursor: pointer;
+         text-align: center;
+         width:100%;
+     }
+     .brands-section {
+         margin-bottom: 100px;
+         display: grid;
+         grid-template-columns: repeat(6, calc( 16.66% - 25px ) );
+         grid-gap: 30px;
+     }
+     h2 a{
+         font-size:1.35rem;
+         color:black;
+     }.img-parent{
+        height:80px;
+     }
+    </style>
+<div class="fs-breadcrumbs-wrapper">
+    <div class="fs-container">
+        <ul class="fs-breadcrumbs-list">
+            <li class="fs-breadcrumbs-el"><a href="/"><?=$GLOBALS['text']['__home__page__']?></a></li>
+            <li class="fs-breadcrumbs-el"><?=$GLOBALS['text']['__brands__']?></li>
+        </ul>
+    </div>
+</div>
+<section>
+    <div class="fs-container brands-section">
+        <?php foreach ($brands as $index => $brand) { ?>
+            <div class="brand-block">
+                <div class="img-parent">
+                    <img src='<?= $brand->logo ?>'>
+                </div>
+                <div>
+                    <h2><a href="brands/<?= $brand->id ?>"><?= $brand->name; ?></a></h2>
+                </div>
+            </div>
+        <?php }    ?>
+        <div>
+</section>
---
+++ views/site/pagination.php
@@ -0,0 +1,58 @@
+<style>
+    .pagination{
+        display:flex;
+        flex-wrap:wrap;
+        list-style:none;
+        margin-top:20px;
+        padding-left:0px;
+        gap:8px;
+    }
+    .pagination a{
+        font-size:18px;
+        text-decoration:none;
+        color:black;
+    }
+    .pagination li.active{
+        background:#ffbd27;
+    }
+    .pagination li.active a{
+        color:white;
+    }
+</style>
+<ul class="pagination w-100">
+    <?php $pages = ceil(intval($total)/20); ?>
+    <?php if(isset($_GET['page']) && intval($_GET['page'] )>0){ ?>
+        <li class="page-item">
+            <a class="page-link" href="<?= Yii::$app->urlManager->createUrl(['/shop']) ?>?page=<?php echo intval($_GET['page'])-1;?>" aria-label="Previous">
+                <span aria-hidden="true">&laquo;</span>
+                <span class="sr-only">Previous</span>
+            </a>
+        </li>
+    <?php } ?>
+    <?php
+    if(!isset($_GET['page'])){
+        $_GET['page'] = 0;
+    }
+    for ($i = 1; $i <= $pages; $i++){
+        if( $i == (intval($_GET['page'])+2) || $i == (intval($_GET['page'])+3) || $i == (intval($_GET['page'])-1)  ||  $i == (intval($_GET['page'])) ||  $i == intval($_GET['page'])+1) {
+            $class = '';
+            if($i == intval($_GET['page'])+1){
+                $class = 'active';
+            }
+            echo '<li class="page-item '.$class.'"><a class="page-link" href="'.Yii::$app->urlManager->createUrl(['/shop']).'?page=' . ($i-1).'">' . $i.'</a></li>';
+        }
+    } ?>
+    <?php if(isset($_GET['page']) && (intval($_GET['page'] )+1) < $pages){ ?>
+        <li class="page-item">
+            <a class="page-link" href="<?= Yii::$app->urlManager->createUrl(['/shop']) ?>?page=<?php echo (intval($_GET['page'] )+1);?>" aria-label="Next">
+                <span aria-hidden="true">&raquo;</span>
+                <span class="sr-only">Next</span>
+            </a>
+        </li>
+    <?php } ?>
+    <li>
+    <li class="page-item"><a class="page-link" href="<?= Yii::$app->urlManager->createUrl(['/shop']) ?>?page=all">Տեսնել ամբողջը</a></li>
+    </li>
+</ul>
+
+
